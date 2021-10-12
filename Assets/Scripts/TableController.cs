db573564972f94e5cfeff4966486761babe17bb4
Jaclyn Ngo <73602536+orange-flower@users.noreply.github.com>
2021-10-12 04:06:54 -0500

    Update TableController.cs

--- Assets/Scripts/TableController.cs
+++ Assets/Scripts/TableController.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TableController : MonoBehaviour
 {
     public Animator slideAnim;
-    public GameObject playerTogGroup;
+    public Toggle player1Tog;
+    public Toggle player2Tog;
     public GameObject player1Panel;
     public GameObject player2Panel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1Panel.SetActive(true);
+        player2Panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ToggleInventory();
     }
 
     public void SlideInTable()
@@ -28,5 +31,19 @@ public class TableController : MonoBehaviour
     public void SlideOutTable()
     {
         slideAnim.SetBool("open", false);
+    }
+
+    private void ToggleInventory()
+    {
+        if (player1Tog.isOn)
+        {
+            player1Panel.SetActive(true);
+            player2Panel.SetActive(false);
+        }
+        else if (player2Tog.isOn)
+        {
+            player1Panel.SetActive(false);
+            player2Panel.SetActive(true);
+        }
     }
 }
